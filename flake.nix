3b70ebe876876e48b4a6e595eede7fb17ceb5e84
Tim Danner <tim@danners.us>
2025-10-18 20:14:56 -0500

    devshell flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Development environment for the vscode-asn1 extension";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+    in
+    {
+      devShells = nixpkgs.lib.genAttrs systems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.nodejs_20
+            ];
+          };
+        });
+    };
+}
